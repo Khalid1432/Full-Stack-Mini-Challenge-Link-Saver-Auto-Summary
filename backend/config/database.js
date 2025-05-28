6015bcc7b88a3b53ac694c8c098f8d702f1b0076
Khalid1432 <khalidjameel145@gmail.com>
2025-05-28 17:30:43 +0530

    update database

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 exports.connect = () => {
-    mongoose.connect(process.env.MONGODB_URI,{
-        useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
+    mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error(err));
 }
